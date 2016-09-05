3200e9be085395638d36b32f4a911bcce1916096
rezkimm <rizki.snutz@gmail.com>
2016-09-05 08:07:46 +0700

    Add files via upload

---
+++ modul_24_arduino_4211501002.ino
@@ -0,0 +1,16 @@
+void setup() {
+pinMode(A0, INPUT);
+digitalWrite(A0, 1);
+pinMode(12, OUTPUT);
+
+}
+
+void loop() {
+if(digitalRead(A0)==0){
+  digitalWrite(12, 1);
+  
+}
+else {
+  digitalWrite(12,0);
+}
+}
